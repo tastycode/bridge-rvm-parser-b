7ac4e81428590f8f85bd02d6540b6c27b52e86a6
tastycode <thomas.devol@gmail.com>
2015-03-21 15:30:25 -0700

    Initial commit

---
+++ url_parser.rb
@@ -0,0 +1,22 @@
+class URLParser
+
+  def initialize(input)
+    @raw = input
+  end
+
+  def self.parse(url)
+    new(url)
+  end
+
+  def scheme
+    parsed[:scheme]
+  end
+
+  def host
+    parsed[:host]
+  end
+
+  def parsed
+    @raw.match(%r{^((?<scheme>[\w]+)://)?(?<host>[\w\-\.]+)?})
+  end
+end
---
+++ url_parser_spec.rb
@@ -0,0 +1,20 @@
+require "./url_parser"
+require 'pry'
+describe URLParser do
+  let(:url) { "http://www.example.com" }
+  let(:parsed) { URLParser.parse(url) }
+
+  describe "with regular strings" do
+    let(:url) { "http://www.example.com" }
+    it "parses the scheme" do
+      expect(parsed.scheme).to eq "http"
+    end
+  end
+
+  describe "with ftp" do
+    let(:url) { "ftp://www.puddle.com" }
+    it "parses the scheme" do
+      expect(parsed.scheme).to eq "ftp"
+    end
+  end
+end
